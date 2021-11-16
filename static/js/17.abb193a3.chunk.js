2c3fefae7582732914c000241003dd64d2d2947e
Wasabi95 <carvajal.ups@gmail.com>
2021-11-16 17:35:34 -0500

    Updates

---
+++ static/js/17.abb193a3.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpamplify=this.webpackJsonpamplify||[]).push([[17],{214:function(t,n,e){"use strict";e.r(n),e.d(n,"amplify_auth_container",(function(){return u}));var i=e(9),u=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("form",{autoComplete:"on",hidden:!0},Object(i.i)("input",{name:"username"}),Object(i.i)("input",{name:"password",type:"password"}),Object(i.i)("input",{type:"submit"})),Object(i.i)("slot",null))},t}()}}]);
+//# sourceMappingURL=17.abb193a3.chunk.js.map
---
+++ static/js/21.69b46fdd.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpamplify=this.webpackJsonpamplify||[]).push([[21],{221:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return c}));var i=n(9),c=function(){function t(t){Object(i.k)(this,t)}return t.prototype.render=function(){return Object(i.i)(i.b,null,Object(i.i)("slot",null))},t}();c.style="amplify-container{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=21.69b46fdd.chunk.js.map
